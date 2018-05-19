6da3266b5b2ffd06d61210bb3ea205d4be274d17
lomic28 <try7loving@gmail.com>
2018-05-19 09:04:42 +0800

    add redux

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'less/main.less';
+
 ReactDOM.render(<div>aaa</div>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
@@ -12,16 +15,21 @@ module.exports = {
     filename: '[name].bundle.js',
     path: `${__dirname}/build`,
     publicPath: '/build/'
-    // webpack-dev-server服务上的文件是相对publicPath这个路径的，用于设置热加载的服务器
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // 启用 HMR
     new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ],
   resolve: {
     // 定义了解析模块路径时的配置，常用的就是extensions，可以用来指定模块的后缀，这样在引入模块时就不需要写后缀了，会自动补全
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    alias: {
+      less: path.resolve(__dirname, 'public/less/')
+    }
   },
   module: {
     rules: [
@@ -34,6 +42,15 @@ module.exports = {
             presets: ['env', 'react', 'es2015']
           }
         }
+      }, {
+        test: /\.less$/,
+        exclude: /(node_modules)/,
+        use: [
+          // MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
       }
     ]
   },
